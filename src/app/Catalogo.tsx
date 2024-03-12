d9c9a1bcddd9587a27d0fcc6fce68649c78f94ea
Henrique Heron <henriqueheron1@gmail.com>
2024-03-11 21:07:39 -0300

    initial commit

--- src/app/Catalogo.tsx
+++ src/app/Catalogo.tsx
@@ -1,30 +1,22 @@
-import { GetServerSideProps } from 'next';
-
-const Catalogo = async ({ data }) => {
-    console.log(data)
-
+const Catalogo = async () => {
+    const APIdata = await pegarAPI()
+  
     return (
-        <div id="main-container">
-            <h1>Characters</h1>
-            <ul>
-                {data?.results.map((result) => (
-                    <li key={result.id}>{result.name}</li>
-                ))}
-            </ul>
-        </div>
+      <div id="main-container">
+        <h1>Characters</h1>
+        <ul>
+          {APIdata?.results.map((result) => (
+            <li key={result.id}>{result.name}</li>
+          ))}
+        </ul>
+      </div>
     );
+  };
+  
+  export default Catalogo;
+
+async function pegarAPI() {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    return data
 }
-
-
-export const getServerSideProps: GetServerSideProps = async () => {
-    const response = await fetch('https://rickandmortyapi.com/api/character')
-    const data = await response.json()
-    console.log(data)
-    return {
-        props: {
-            data: data
-        }
-    }
-}
-
-export default Catalogo
