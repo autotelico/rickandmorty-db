8871779d4be06c619dd57fb125aeb70629588141
Henrique Heron <henriqueheron1@gmail.com>
2024-03-14 12:53:43 -0300

    remove and add comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 // Página principal
+// Esta página renderiza os personagens e permite ver detalhes sobre
+// cada um. Também é possível pesquisar personagens.
 
-'use client';
-import { ChangeEvent, Suspense, useState, useEffect } from 'react';
-import Characters from './characters/Characters';
-import './globals.css'
+"use client";
+import { ChangeEvent, Suspense, useState, useEffect } from "react";
+import Characters from "./characters/Characters";
+import "./globals.css";
 
 export default function Page(): JSX.Element {
   const [isClient, setIsClient] = useState<boolean>(false); // Verifica se tem window
   const [page, setPage] = useState<number>(1); // Página atual
-  const [character, setCharacter] = useState<string>(''); // Personagem buscado
+  const [character, setCharacter] = useState<string>(""); // Personagem buscado
 
   useEffect(() => {
     setIsClient(true);
@@ -37,7 +39,7 @@ export default function Page(): JSX.Element {
           id="main-icon"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Rick_and_Morty.svg/800px-Rick_and_Morty.svg.png"
           alt=""
-        />{' '}
+        />{" "}
         <p id="dti-love">and dti s2💞✨✨✨</p>
       </header>
       {/* Se houver window, ele vai renderizar isto */}
@@ -46,7 +48,7 @@ export default function Page(): JSX.Element {
           <div id="hero">
             <div id="hero-text">
               <h2>Filtro de Personagens</h2>
-              <h4 style={{ fontWeight: 'normal' }}>
+              <h4 style={{ fontWeight: "normal" }}>
                 Confira os principais dados dos personagens de Rick and Morty,
                 como seu nome, seu status, e muito mais abaixo!
               </h4>
@@ -75,11 +77,12 @@ export default function Page(): JSX.Element {
           <h3>Page {page}/42</h3>
         </div>
         <div id="character-list">
-          <Suspense fallback={<p style={{ fontSize: '32px' }}>Loading...</p>}>
+          <Suspense fallback={<p style={{ fontSize: "32px" }}>Loading...</p>}>
             <Characters page={page} character={character} />
           </Suspense>
         </div>
       </div>
+      {/* Botão extra pra voltar ao topo mais rápido */}
       <button
         id="scroll-to-top-btn"
         onClick={() => isClient && window.scrollTo(0, 0)}
